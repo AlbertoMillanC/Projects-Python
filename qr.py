4e6e1e0f5467cf0c0c23e505a50da8ffe93e5acd
Alberto Millán <90913818+AlbertoMillanC@users.noreply.github.com>
2022-05-31 00:00:00 -0500

    codigos qr

---
+++ qr.py
@@ -0,0 +1,18 @@
+# generar codigo qr
+
+
+import qrcode
+
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+
+qr.add_data('http://www.google.com')
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="black", back_color="white")
+
+img.save('qr.png')
